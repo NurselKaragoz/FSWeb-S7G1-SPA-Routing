0c0560f1579451898b96622025fc6c5de5644fc1
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-08-07 19:56:27 +0300

    axiosdan gelen datadan filmler listelendi

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
+import { BrowserRouter } from "react-router-dom/";
 
-export default function App () {
+import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
+
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
-        .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
+        .get("http://localhost:5001/api/filmler") // Burayı Postman'le çalışın
+        .then((response) => {
+          setMovieList(response.data);
+          console.log(response);
+
+          // Perform any operations or transformations on each item.
+
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
-        .catch(error => {
-          console.error('Sunucu Hatası', error);
+        .catch((error) => {
+          console.error("Sunucu Hatası", error);
         });
-    }
+    };
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
+  const KaydedilenlerListesineEkle = (id) => {
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
+      <KaydedilenlerListesi
+        list={
+          [
+            /* Burası esnek */
+          ]
+        }
+      />
+      <ul>
+        {movieList.map((item) => (
+          <li key={item.id}>
+            {item.title} {item.id}
+          </li>
+        ))}
+      </ul>
 
       <div>Bu Div'i kendi Routelarınızla değiştirin</div>
     </div>
